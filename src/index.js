ace06b6bf3f0f282c4b1b1109cf26c054420d972
john7319 <jonniekaras@gmail.com>
2024-04-01 15:37:50 +0300

    Added a js function to change the value of movie title and description when you enter the id

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
+  const inputForm = document.querySelector("form")
+  inputForm.addEventListener("submit",(event)=> {
+    event.preventDefault()
+    const inputValue = event.target.children[1].value
+
+    fetch(`http://localhost:3000/movies/${inputValue}`)
+    .then(ftch => ftch.json())
+    .then(data => {
+        const title = document.querySelector('section#movieDetails h4')
+        const summary = document.querySelector('section#movieDetails p')
+
+        title.innerHTML = data.title
+        summary.innerHTML = data.summary
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
